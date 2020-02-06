4cf0c6c5eb1eceb2a8d571b6eda5f1d04be89688
Josiah <reikiryo@yahoo.com>
2020-02-05 23:50:52 -0500

    minor style changes *removed focused styes from text inputs

--- kids-fly/src/components/Login/LoginForm.js
+++ kids-fly/src/components/Login/LoginForm.js
@@ -7,10 +7,10 @@ const Login = () => {
     return (  
         <div><h1 className='input-h1'>Login</h1>
             <form>
-                <input className='text-input' type='text' placeholder='Email' /><br/>
-                <input className='text-input' type='text' placeholder='Password' /><br/>
+                <input className='regi-text-input' type='text' placeholder='Email' /><br/>
+                <input className='regi-text-input' type='text' placeholder='Password' /><br/>
                 <Link to='/'><button className='input-button'>Go Back</button></Link>
-                <button className='input-button'>Login</button>
+                <button className='regi-input-button'>Login</button>
             </form>
         </div>
     );
