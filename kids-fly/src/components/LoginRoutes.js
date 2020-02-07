9829b6cbe777e1a211875fa3bf7d06c4cd0c7367
Josiah <reikiryo@yahoo.com>
2020-02-07 11:58:33 -0500

    removes last commit

--- kids-fly/src/components/LoginRoutes.js
+++ kids-fly/src/components/LoginRoutes.js
@@ -1,27 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import Login from './LoginForm'
-import axios from 'axios'
 import RegisterForm from './RegisterForm'
 import Register from './Register'
 import { Link, Route } from 'react-router-dom'
 
 function Routes() {
-  const [data, setData] = useState('')
-  const api = `http://numbersapi.com/random/math`
-  useEffect(() => {
-    axios.get(api)
-    .then(res => {
-      setData(res.data)
-    })
-    .catch(err => {
-      console.log("ERROR", err)
-    })
-  }, [])
-
-
   return (
     <div className="login-div">
-    <p>{data}</p>
       <Route exact path='/'>
         <Link className='login-link' to="/login" ><h1 className='login-h1'>Login</h1></Link><br/>
         <Link className='login-link' to='/register' ><h1 >Register</h1></Link>
