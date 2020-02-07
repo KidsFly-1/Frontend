7f1e8be0e27c4f529def42128254960104e3a4d6
Josiah <reikiryo@yahoo.com>
2020-02-07 12:24:08 -0500

    Adds axios call to loginform.js

--- kids-fly/src/components/LoginForm.js
+++ kids-fly/src/components/LoginForm.js
@@ -4,8 +4,19 @@ import { Link } from 'react-router-dom'
 import {connect} from 'react-redux'
 import {login} from '../actions/login'
 
-const Login = props => {
-    const [user, setUser] = useState({ 
+const Login = () => {
+    const [data, setData] = useState('')
+    const api = `http://numbersapi.com/random/math`
+    useEffect(() => {
+        axios.get(api)
+            .then(res => {
+                setData(res.data)
+            })
+            .catch(err => {
+                console.log("ERROR", err)
+            })
+    }, [])
+    const [user, setUser] = useState({
         email: '',
         password: ''
     })
@@ -19,13 +30,15 @@ const Login = props => {
         props.login(values)
     }
 
-    return (  
-        <div><h1 className='input-h1'>Login</h1>
+    return (
+        <div>
+        <p>{data}</p>
+        <h1 className='input-h1'>Login</h1>
             <form onSubmit={handleSubmit}>
-                <input onChange={handleChange} name='email' className='regi-text-input' type='text' value={user.email} placeholder='Email' required /><br/>
-                <input onChange={handleChange} name='password' className='regi-text-input' type='password' placeholder='Password' required /><br/>
-                <Link to='/'><button className='regi-input-button'>Go Back</button></Link>
-                <button type="submit" className='regi-input-button'>Login</button>
+                <input onChange={handleChange} name='email' className='text-input' type='text' value={user.email} placeholder='Email' required /><br />
+                <input onChange={handleChange} name='password' className='text-input' type='password' placeholder='Password' required /><br />
+                <Link to='/'><button className='input-button'>Go Back</button></Link>
+                <button type="submit" className='input-button'>Login</button>
             </form>
         </div>
     );
